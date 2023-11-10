5901105bbc20ddfdb7710aae61eeeeef3c99104a
DimtryK <dimakonichev@gmail.com>
2023-11-10 12:39:53 +0300

    commit

---
+++ set_configuration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+
+if ! grep -q "nginx-server" trusted_domain.tmp; then
+    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
+    docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+fi
+
+rm trusted_domain.tmp
+
+docker exec -u www-data nextcloud-local php occ --no-warnings app:install onlyoffice
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-local/"
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set onlyoffice jwt_secret --value="O9aFioQOAxc4SB4P1IaKe6JOyiq5WgCM"
+docker exec -u www-data nextcloud-local php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
